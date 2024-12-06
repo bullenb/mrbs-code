30ccbbc0b3717625aa02907596a3c595e3a01179
Brendan Bullen <66941+bullenb@users.noreply.github.com>
2024-12-06 12:55:42 +0000

    Add NMH specific auth
    Signed-off-by: Brendan Bullen <66941+bullenb@users.noreply.github.com>

--- web/lib/MRBS/Auth/AuthLdapNmh.php
+++ web/lib/MRBS/Auth/AuthLdapNmh.php
@@ -4,7 +4,7 @@ namespace MRBS\Auth;
 
 use MRBS\User;
 
-require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../../../vendor/autoload.php';
 
 class AuthLdapNmh extends AuthLdap
 {
